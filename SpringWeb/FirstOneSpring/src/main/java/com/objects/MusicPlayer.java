bbed666b5a51046816052588475f5cac284d6cff
Николай Зотин <15nk33l49@gmail.com>
2022-03-06 17:30:56 +0300

    -

--- SpringWeb/FirstOneSpring/src/main/java/com/objects/MusicPlayer.java
+++ SpringWeb/FirstOneSpring/src/main/java/com/objects/MusicPlayer.java
@@ -1,11 +1,38 @@
 package com.objects;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class MusicPlayer {
-    private Music music;
+    private List<Music>musicList= new ArrayList<Music>();
+    private String name;
+    private int volume;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
 
     public MusicPlayer(Music music) {
         this.music = music;
     }
+    public MusicPlayer(){}
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
     public void playMusic(){
         System.out.println("Playing "+music.getSong());
     }
--- SpringWeb/FirstOneSpring/src/main/java/com/objects/TestSpring.java
+++ SpringWeb/FirstOneSpring/src/main/java/com/objects/TestSpring.java
@@ -6,10 +6,13 @@ public class TestSpring {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context= new ClassPathXmlApplicationContext(
                 "applicationContext.xml");
-        Music music= context.getBean("musicBean",Music.class);
-        MusicPlayer musicPlayer= new MusicPlayer(music);
+//        Music music= context.getBean("musicBean",Music.class);
+//        MusicPlayer musicPlayer= new MusicPlayer(music);
+        MusicPlayer musicPlayer= context.getBean("musicPlayer",MusicPlayer.class);
 
         musicPlayer.playMusic();
+        System.out.println(musicPlayer.getVolume());
+        System.out.println(musicPlayer.getName());
 
         context.close();
     }
