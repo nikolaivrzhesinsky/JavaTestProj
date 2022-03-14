a572f41ba9d4af551ec338e03fc547abbd8300c4
Николай Зотин <15nk33l49@gmail.com>
2022-03-14 22:43:35 +0300

    анотации, реализация рандомного воспроизведения музыки

--- SpringWeb/SpringAnotation/src/main/java/com/objects/TestSpring.java
+++ SpringWeb/SpringAnotation/src/main/java/com/objects/TestSpring.java
@@ -1,20 +1,14 @@
 package com.objects;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import java.sql.SQLOutput;
 
 public class TestSpring {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext context= new ClassPathXmlApplicationContext(
-                "applicationContext.xml");
 
-
-
-//        MusicPlayer musicPlayer=context.getBean("musicPlayer",MusicPlayer.class);
-//
-//        System.out.println(musicPlayer.getName());
-//        System.out.println(musicPlayer.getVolume());
+        AnnotationConfigApplicationContext context= new AnnotationConfigApplicationContext(SpringConfig.class);
 
        Computer computer=context.getBean("computer",Computer.class);
         System.out.println(computer);
